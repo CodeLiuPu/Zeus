77eb723b4323235f6a2a2efa2832186553856a7f
liupu <575866447@qq.com>
2019-07-01 17:42:32 +0800

    plugin TestService

--- app/src/main/java/com/update/zeus/app/App.java
+++ app/src/main/java/com/update/zeus/app/App.java
@@ -3,22 +3,33 @@ package com.update.zeus.app;
 import android.app.Application;
 import android.content.Context;
 
+import com.update.zeus.helper.BaseDexClassLoaderHookHelper;
+import com.update.zeus.helper.Utils;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * @author : liupu
  * date   : 2019/6/26
  * desc   :
  */
 public class App extends Application {
-    private static Context sContext;
+    private static final String apkName = "plugin-debug.apk";
+    private static final String dexName = "plugin-debug.dex";
 
     @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        sContext = base;
-    }
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+        Utils.extractAssets(newBase, apkName);
+        File dexFile = getFileStreamPath(apkName);
+        File optDexFile = getFileStreamPath(dexName);
 
-    public static Context getContext() {
-        return sContext;
+        try {
+            BaseDexClassLoaderHookHelper.patchClassLoader(getClassLoader(),dexFile,optDexFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- plugin/src/main/java/com/update/plugin/service/TestService.java
+++ plugin/src/main/java/com/update/plugin/service/TestService.java
@@ -3,8 +3,22 @@ package com.update.plugin.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class TestService extends Service {
+    private static final String TAG = "TestService";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e(TAG,"onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG,"onDestroy");
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
